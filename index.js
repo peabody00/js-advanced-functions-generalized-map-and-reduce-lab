98ce89cc2068d14a487a97dc44fec3755da027aa
Charlie <peabody00@gmail.com>
2021-06-10 12:39:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your functions here
+function map(sourceArray, fn) {
+    let newArray = [];
+    sourceArray.forEach(element => {
+        newArray.push(fn(element));
+    })
+    return newArray;
+}
+
+function reduce(sourceArray, callback, startingPoint) {
+    let total;
+    
+    if (startingPoint) {
+        total = startingPoint;
+        for (let i = 0; i < sourceArray.length; i++) {
+        total = callback(sourceArray[i], total);
+        }
+        return total;
+    } 
+    else {
+        total = sourceArray[0];
+        for (let i = 1; i < sourceArray.length; i++) {
+        total = callback(sourceArray[i], total);
+        }
+        return total;
+    }
+}
